98c094ba6704b2f450516eb96ac8aed35e3fbbe0
Huras Alexandre Ferreira dos Santos Gomes <hurat@gmail.com>
2019-12-09 08:35:35 -0200

    feat: monsters types

--- monster_system.js
+++ monster_system.js
@@ -1,5 +1,11 @@
 let Monster = class {
 
     constructor(){
+        const monsters = {
+            SLIME: 'slime',
+            GIANT_WOLF: 'Giant Wolf',
+            GIANT_SPIDER: 'Giant Spider',
+            GIANT_BAT: 'Giant Bat',
+        }
     }
 }
