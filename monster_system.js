b9a817ae2f938205237412af7e617d614f59126d
Huras Alexandre Ferreira dos Santos Gomes <hurat@gmail.com>
2019-12-09 08:56:46 -0200

    refactor: change monster constructor + change monster type enum scope

--- monster_system.js
+++ monster_system.js
@@ -1,14 +1,14 @@
+const monsters = {
+    UNKNOWN: 'unknown',
+    SLIME: 'slime',
+    GIANT_WOLF: 'Giant Wolf',
+    GIANT_SPIDER: 'Giant Spider',
+    GIANT_BAT: 'Giant Bat',
+}
+
 let Monster = class {
 
-    constructor(){
-        const monsters = {
-            UNKNOWN: 'unknown',
-            SLIME: 'slime',
-            GIANT_WOLF: 'Giant Wolf',
-            GIANT_SPIDER: 'Giant Spider',
-            GIANT_BAT: 'Giant Bat',
-        }
-
-        this.type = monsters.UNKNOWN;
+    constructor(type = monsters.UNKNOWN){
+        this.type = type;
     }
 }
