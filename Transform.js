f719103250418f8b9418ea697a79da09a14b0959
Huras Alexandre <hurast@gmail.com>
2019-12-12 20:14:13 -0300

    feat: Transform

---
+++ Transform.js
@@ -0,0 +1,11 @@
+const Vector3 = require("./Vector3");
+
+let Transform = class {
+  constructor() {
+    this.position = new Vector3.class();
+  }
+
+  translate() {}
+};
+
+module.exports = { class: Transform };
